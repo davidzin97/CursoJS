8b0adc55ce8f427dedc0e6a315ccee5cb2276b4e
davidGomes <davidzin97@gmail.com>
2024-01-30 20:00:48 -0300

    ok
    ok

--- analisador de numero/script.js
+++ analisador de numero/script.js
@@ -1,16 +1,16 @@
 function analisarNumero() {
-    const numero = document.getElementById('numero').value
-    const resultado = document.getElementById('resultado')
+    const numero = document.getElementById('numero').value;
+    const resultado = document.getElementById('resultado');
 
     if (isNaN(numero)) {
-        resultado.textContent = 'Por favor, digite um número válido.'
+        resultado.textContent = 'Por favor, digite um número válido.';
     } else {
-        if (numero % 2 === 0){
-            resultado.textContent = `${numero} é um número Par.`
-        } else if {
-            resultado.textContent = `${numero} é um número Impar.`
+        if (numero == 0) {
+            resultado.textContent = 'Você digitou zero.';
+        } else if (numero % 2 === 0) {
+            resultado.textContent = `${numero} é um número Par.`;
         } else {
-            resultado.textContent = 'Você digitou zero.'
+            resultado.textContent = `${numero} é um número Ímpar.`;
         }
     }
 }
